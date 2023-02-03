91900a3fbcaf4b14edb2b816d5384f86b19d7365
Crystal Campbell <blankpage@gmail.com>
2023-02-03 11:52:15 -0600

    exercise 2

---
+++ config/routes.rb
@@ -0,0 +1,28 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  root "pages#homepage"
+
+  get "/about_us", to: "pages#about_us"
+
+  get "/contact_us", to: "pages#contact_us"
+
+  get "/faq", to: "pages#faq"
+
+  get "/terms_of_service", to: "pages#terms_of_service"
+
+  get "/privacy_policy", to: "pages#privacy_policy"
+
+  get "/course_catalog", to: "pages#course_catalog"
+
+  get "/testimonials", to: "pages#testimonials"
+
+  get "/blogs", to: "blogs#index"
+
+  get "/blogs/:id", to: "blogs#show"
+
+  resourses :blogs
+end
